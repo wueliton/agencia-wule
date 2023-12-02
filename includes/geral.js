8740a9eb949a88236e1096d05caf87785ec0e442
wueliton <wueliton@users.noreply.github.com>
2023-12-02 03:08:12 +0000

    deploy: ec51925d18d060ae5918b47e4b8ea865ebee8793

--- includes/geral.js
+++ includes/geral.js
@@ -1,4 +1,11 @@
+import crossEnv from 'cross-env';
+
+const url = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5500/dist/' : 'https://wueliton.github.io/agencia-wule/';
+
 export const geral = {
     empresa: 'Wule Agência Digital',
-    siteUrl: 'http://127.0.0.1:5500/dist/'
+    slogan: 'Especialistas em SEO e Otimização de Sites',
+    siteUrl: url,
+    logo: `${url}images/logo.png`,
+    phone: '(11) 94004-3902'
 }
